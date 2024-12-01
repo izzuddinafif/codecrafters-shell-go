b7beeeac6ae644552be10c3b5aaf9f245f1dee09
afif <izzuddinafif@gmail.com>
2024-12-01 22:22:55 +0700

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,15 +10,25 @@ import (
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
 
-func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
-
+// REPL is Read, Eval and Print Loop that reads user input,
+// prints the result and wait for the next input.
+func REPL() (err error) {
 	// Wait for user input
 	input, _, err := bufio.NewReader(os.Stdin).ReadLine()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	fmt.Printf("%s: not found\n", input)
+	return nil
+}
 
+func main() {
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		err := REPL()
+		if err != nil {
+			log.Println(err)
+			break
+		}
+	}
 }
